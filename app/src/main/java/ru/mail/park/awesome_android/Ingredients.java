db802bb5ad14f2246df786fa082c06fd837de11f
evgenia <jenushkanor@gmail.com>
2018-04-12 12:16:45 +0300

    Add gson and search button

---
+++ app/src/main/java/ru/mail/park/awesome_android/Ingredients.java
@@ -0,0 +1,16 @@
+package ru.mail.park.awesome_android;
+
+
+import java.util.ArrayList;
+
+public class Ingredients {
+    private ArrayList ingredients = new ArrayList();
+
+    public ArrayList getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList ingredientsArray) {
+        this.ingredients = ingredientsArray;
+    }
+}
--- app/src/main/java/ru/mail/park/awesome_android/MainActivity.java
+++ app/src/main/java/ru/mail/park/awesome_android/MainActivity.java
@@ -3,6 +3,7 @@ package ru.mail.park.awesome_android;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewManager;
@@ -13,6 +14,8 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
@@ -21,8 +24,11 @@ import java.util.HashSet;
 public class MainActivity extends AppCompatActivity {
 
     private Button addButton;
+    private Button searchButton;
     private EditText enterIngredient;
     private LinearLayout addedIngredients;
+
+    private Ingredients ingredients = new Ingredients();
     private ArrayList ingredientsArray = new ArrayList();
 
     private String ingredient;
@@ -34,9 +40,26 @@ public class MainActivity extends AppCompatActivity {
             LinearLayout childLayoutWithButton = (LinearLayout)v.getParent();
             TextView childText = (TextView)parent.getChildAt(0);
 
+            String ingr = childText.getText().toString();
+
+            ingredientsArray.remove(ingr);
+
             ((ViewManager)parent).removeView(childText);
             ((ViewManager)parent).removeView(childLayoutWithButton);
             ((ViewManager)parent).removeView(parent);
+        }
+    };
+
+    private View.OnClickListener onSearchButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Gson gson = new Gson();
+
+            ingredients.setIngredients(ingredientsArray);
+            gson.toJson(ingredients);
+
+//            Log.i("GSON", gson.toJson(ingredients));
+
         }
     };
 
@@ -45,10 +68,12 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
 
             ingredient = enterIngredient.getText().toString();
+            ingredientsArray.add(ingredient);
+
             if (ingredient.length() == 0)  {
                 return;
             }
-            enterIngredient.setText(""); //убрать хардкод
+            enterIngredient.setText(R.string.delete); //убрать хардкод
 
             LinearLayout layoutWithIngredientAndButton = new LinearLayout(getBaseContext());
             layoutWithIngredientAndButton.setOrientation(LinearLayout.HORIZONTAL);
@@ -57,11 +82,10 @@ public class MainActivity extends AppCompatActivity {
             layoutWithButton.setGravity(Gravity.RIGHT);
 
             Button remove = new Button(getBaseContext());
-            remove.setText("-"); //убрать хардкод
+            remove.setText(R.string.remove); //убрать хардкод
             remove.setWidth(200);
             remove.setOnClickListener(onRemoveButtonClickListener);
             layoutWithButton.addView(remove);
-
 
 
             TextView text = new TextView(getBaseContext());
@@ -85,10 +109,11 @@ public class MainActivity extends AppCompatActivity {
 
         enterIngredient = (EditText)findViewById(R.id.ingredient);
         addButton = (Button)findViewById(R.id.add_button);
-
+        searchButton = (Button)findViewById(R.id.search_recipe);
 
         addedIngredients = (LinearLayout) findViewById(R.id.add_ingredient_layout);
 
         addButton.setOnClickListener(onAddButtonClickListener);
+        searchButton.setOnClickListener(onSearchButtonClickListener);
     }
 }
