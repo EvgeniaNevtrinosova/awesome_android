08edd135bd040edd802f54f1e6342947f8a9cc3e
evgenia <jenushkanor@gmail.com>
2018-04-27 19:25:03 +0300

    Add splash screen

---
+++ app/src/main/java/ru/mail/park/awesome_android/SplashActivity.java
@@ -0,0 +1,22 @@
+package ru.mail.park.awesome_android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
